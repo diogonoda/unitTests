2b7f82fda01a614bf1656aacedf1169879b8a215
Diogo Noda <diogonoda@hotmail.com>
2016-05-30 00:10:16 -0300

    Compiladas as novas rotinas

---
+++ lib/pc_departments.sql
@@ -0,0 +1,33 @@
+create or replace package pc_departments
+ as
+  function get_department(p_id  in  departments.dept_id%type)
+    return departments%rowtype;
+end pc_departments;
+/
+--
+--
+create or replace package body pc_departments
+ as
+  function get_department(p_id  in  departments.dept_id%type)
+    return departments%rowtype
+   as
+    v_department  departments%rowtype;
+  begin
+    select dept_id,
+           dept_name,
+           dept_sta_bonus
+      into v_department.dept_id,
+           v_department.dept_name,
+           v_department.dept_sta_bonus
+      from departments
+     where dept_id = 2;
+    --
+    return v_department;
+  exception
+    when no_data_found then
+      return null;
+    when others then
+      raise_application_error(-20101, sqlerrm);
+  end get_department;
+end pc_departments;
+/
